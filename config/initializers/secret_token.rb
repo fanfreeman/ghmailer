7505aa15cca7ac5feb8a918ee6a3266adbb14363
Freeman Fan <fanfreeman@gmail.com>
2013-04-23 00:10:49 -0700

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubMailer::Application.config.secret_token = '41151b6f8735c2c10e9df5d935648cf187f31ed236bdfffc04a986117c69ca96b035df6a23f2415e08468d8be4ecb84656bac5eb97e2f854c6df133043fb22dc'
